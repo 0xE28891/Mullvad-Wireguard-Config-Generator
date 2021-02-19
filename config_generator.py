a9d4066b8ee35fe1f812158440819c3579a74365
0xE28891 <77920271+0xE28891@users.noreply.github.com>
2021-02-19 13:37:26 +0000

    Update config_generator.py

--- config_generator.py
+++ config_generator.py
@@ -1,18 +1,11 @@
-# -*- coding: utf-8 -*-
 """
-Usage: python3 mullvad_config_generator.py
+Usage: python3 config_generator.py
 
 Fetches all Wireguard servers from Mullvad's public API
 Generates a wireguard .conf file for each Wireguard server
 that Mullvad has available.
 
-Config files are saved to /MullvadConfigs
-
-This script is useful because you may set your own DNS
-server, and I think it's faster and more convenient to
-run this script to get the latest servers (and delete old
-/retired ones). I also think the naming convention is better
-for the config files are better
+Config files are saved to /etc/wireguard/mullvad
 """
 
 import pathlib
@@ -23,7 +16,7 @@ MULLVAD_SERVER_LIST_ENDPOINT = "https://api.mullvad.net/www/relays/all/"
 ADDRESS = "10.66.166.115/32"
 PRIVATE_KEY = ""
 DNS = "193.138.218.74"
-IPTABLES = "PostUp = iptables -t nat -A POSTROUTING -o %i -j MASQUERADE\nPostUp = iptables -I FORWARD -i vpn0 -o %i -j ACCEPT\nPostUp = ip rule add from 10.10.10.0/24 lookup 55111\nPostUp = ip rule add lookup main suppress_prefixlength 0\nPreDown = iptables -t nat -D POSTROUTING -o %i -j MASQUERADE\nPreDown = ip rule del lookup main suppress_prefixlength 0\nPreDown = ip rule del from 10.10.10.0/24 lookup 55111"
+IPTABLES = "Table = 55111\n\nPostUp = iptables -t nat -A POSTROUTING -o %i -j MASQUERADE\nPostUp = iptables -I FORWARD -i vpn0 -o %i -j ACCEPT\nPostUp = ip rule add from 10.10.10.0/24 lookup 55111\nPostUp = ip rule add lookup main suppress_prefixlength 0\nPreDown = iptables -t nat -D POSTROUTING -o %i -j MASQUERADE\nPreDown = ip rule del lookup main suppress_prefixlength 0\nPreDown = ip rule del from 10.10.10.0/24 lookup 55111"
 FILE_ILLEGAL_CHARS = r"/?:\<>*|#, "
 
 CONFIG_DIRECTORY = f"/etc/wireguard/mullvad"
@@ -54,7 +47,6 @@ def generate_wireguard_config(jsondata): #generates a wireguard config string, g
     configstring += f"PrivateKey = {PRIVATE_KEY}\n"
     configstring += f"Address = {ADDRESS}\n"
     configstring += f"DNS = {DNS}\n"
-    configstring += f"Table = 55111\n\n"
     configstring += f"{IPTABLES}\n\n"
     configstring += "[Peer]\n"
     configstring += f"PublicKey = {jsondata['pubkey']}\n"
@@ -73,11 +65,13 @@ if __name__ == "__main__":
     server_data = server_data_request.json()
 
     for item in server_data:
+        active_status = item.get("active")
+        owned_status = item.get("owned")
         server_type = item.get("type")
-        country_code = item.get("country_code")
+        country_code = item.get("country_code")     
         
-        if server_type == "wireguard" and country_code != "au" and country_code != "br" and country_code != "ca" and country_code != "gb" and country_code != "hk" and country_code != "jp" and country_code != "nz" and country_code != "sg" and country_code != "us":
-            #print(item)
+        
+        if active_status == True and owned_status == True and server_type == "wireguard" and country_code != "au" and country_code != "br" and country_code != "ca" and country_code != "gb" and country_code != "hk" and country_code != "jp" and country_code != "nz" and country_code != "sg" and country_code != "us":
             save_config_to_file(item)
 
-    print(f"Saved {len(server_data)} config files to {CONFIG_DIRECTORY}")
+    print(f"Saved config files to {CONFIG_DIRECTORY}")
